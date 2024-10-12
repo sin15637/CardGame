fed499605f16d3a8a70569edb1443811259b2b2c
himanisharma12 <himani12@sheridancollege.ca>
2024-10-12 17:13:37 -0400

    Starter Code - Added Name and Date.

---
+++ ProjectDeliverable1/src/projectDeliverable1/Card.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Himani, Hamanjot Kaur Mundi, Anmoldeep Singh Hanzra, Nishant.
+ * Team Name: Game Ninjas
+ * @Date: 2024-10-12
+ */
+package projectDeliverable1;
+
+/**
+ * A class to be used as the base Card class for the project. Must be general enough to be instantiated for any Card
+ * game. Students wishing to add to the code should remember to add themselves as a modifier.
+ *
+ * @author dancye
+ */
+public abstract class Card {
+    //default modifier for child classes
+
+    /**
+     * Students should implement this method for their specific children classes
+     *
+     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
+     */
+    @Override
+    public abstract String toString();
+
+}
---
+++ ProjectDeliverable1/src/projectDeliverable1/Player.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projectDeliverable1;
+
+/**
+ *
+ * @author Himani, Hamanjot Kaur Mundi, Anmoldeep Singh Hanzra, Nishant.
+ * Team Name: Game Ninjas
+ * @Date: 2024-10-12
+ */
+public abstract class Player {
+
+    private String name; //the unique name for this player
+
+    /**
+     * A constructor that allows you to set the player's unique ID
+     *
+     * @param name the unique ID to assign to this player.
+     */
+    public Player(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the player name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Ensure that the playerID is unique
+     *
+     * @param name the player name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
+     * with logic to play your game.
+     */
+    public abstract void play();
+
+}
